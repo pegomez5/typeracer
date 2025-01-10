f1de5f6df6fc05f4c809a7e5f0dd549be713d3f5
Pedro <pegomez@ursinus.edu>
2025-01-09 21:50:57 -0500

    Created a react component for progress bars, which updated in real time.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+// TODO : Change this function so that progress is set to
+//        letters typed / length of the paragraph
+function ProgressBar({ interval, maxValue }) {
+  const [cur_progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const startTime = Date.now();
+    const timer = setInterval(() => {
+      const elapsedTime = (Date.now() - startTime) / 1000; // time in seconds
+      setProgress(Math.min(elapsedTime * 10, maxValue)); 
+    }, interval);
+
+    return () => clearInterval(timer); // Cleanup interval on unmount
+  }, [interval, maxValue]);
+
+  return (
+    <progress value={cur_progress} max={maxValue}></progress>
+  );
+}
 
 function App() {
   return (
@@ -16,8 +37,8 @@ function App() {
         <div className="top-panel">
           <h2>Player Progress:</h2>
           <div className="progress-container">
-            <div className="player-progress">Player 1: 50%</div>
-            <div className="player-progress">Player 2: 30%</div>
+            <ProgressBar interval={250} maxValue={100}/>
+            <ProgressBar interval={250} maxValue={100}/>
           </div>
         </div>
 
